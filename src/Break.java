739fd14787fc60b7398de5f76131ffcd25edc0ec
bond2019 <261593968@qq.com>
2019-03-07 20:51:27 +0500

    Initial commit

---
+++ src/Break.java
@@ -0,0 +1,16 @@
+import java.math.BigDecimal;
+
+public class Break {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <3; i++)
+            for (int j = 0; j < 3; j++)
+                for(int k = 0; k < 3; k++) {
+                    if (k == 2) break;
+                    System.out.println(i);
+                }
+
+
+
+    }
+}
